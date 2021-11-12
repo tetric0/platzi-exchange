37b1e23e2877fca5e15dbd2e220e2d061a5d711b
developer <developer@deb11-desktop-base.dev>
2021-11-12 12:34:52 +0100

    Implementacion del spinner de la libreria de terceros 'Saeris/vue-spinners'. ATENCION: Para que ChartKick funcione en Vue2 la libreria Chart se tiene que importar mediante el comando 'import Chart from chart.js/auto;'

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 import { VueSpinners } from "@saeris/vue-spinners";
+import Chart from "chart.js/auto";
+import Chartkick from "vue-chartkick";
 
 Vue.use(VueSpinners);
+Vue.use(Chartkick.use(Chart));
 
 import router from "@/router";
 import { dollarFilter, percentFilter } from "@/filters";
--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex-col">
-    <template v-if="asset.id">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+      <px-assets-table v-if="!isLoading" :assets="assets" />
+    </div>
+
+    <template v-if="!isLoading">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
@@ -95,6 +100,7 @@ export default {
 
   data() {
     return {
+      isLoading: false,
       asset: {},
       history: [],
     };
@@ -127,13 +133,14 @@ export default {
   methods: {
     getCoin() {
       const id = this.$route.params.id;
+      this.isLoading = true;
 
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-        ([asset, history]) => {
+      Promise.all([api.getAsset(id), api.getAssetHistory(id)])
+        .then(([asset, history]) => {
           this.asset = asset;
           this.history = history;
-        }
-      );
+        })
+        .finally(() => (this.isLoading = false));
     },
   },
 };
